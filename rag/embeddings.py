f91f7b39212b29cedae7379adfb1905e7bc8d460
HarshithaSH05 <shharshitha05@gmail.com>
2026-02-13 10:04:37 +0530

    Add embedding function using SentenceTransformer

---
+++ rag/embeddings.py
@@ -0,0 +1,7 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed(texts):
+    return np.array(model.encode(texts))
